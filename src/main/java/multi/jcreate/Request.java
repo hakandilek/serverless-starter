aa1c0ee4ae7daeeb3fb546d7734788a2d9efe329
Hakan Dilek <hakandilek@gmail.com>
2016-05-06 17:37:52 +0200

    generate multi/jcreate function

---
+++ src/main/java/multi/jcreate/Request.java
@@ -0,0 +1,21 @@
+package multi.jcreate;
+
+public class Request {
+
+	private String input;
+
+	public Request(String input) {
+		this.input = input;
+	}
+
+	public Request() {
+	}
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+}
